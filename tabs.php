ac69b688c49393fa655deee9f45ba7f1826a3f2f
Chris Winters <tribalNerd@users.noreply.github.com>
2014-08-19 15:32:06 -0700

    Update tabs.php

--- tabs.php
+++ tabs.php
@@ -2,35 +2,37 @@
 /**
  * Display Navigation Tabs within Wordpress Plugin & Theme Admin Areas
  * Modify: $default_tab and the $tabs array()
- * Call: <?php echo example_tabs();?>
- * @author Chris Winters, Wordpress
- * @version 0.1.0
+ * Call: <?php echo tabs();?>
+ * @author Chris Winters
+ * @version 0.1.1
  */
-function example_tabs() {
+function tabs() {
+    // Required
     if( !isset( $_GET['page'] ) ) { return; }
-
-    // Default Tab from array key
-    $default_tab = 'home';
 
     // Tabs Names: &tab=home
     $tabs = array( 
         'home'      => 'Home', 
         'settings'  => 'Settings', 
         'contact'   => 'Contact' 
-    );
+    );    
+
+    // Required for foreach
+    if( !empty( $tabs ) && !is_array( $tabs ) ) { return; }
 
     // Get page & current tab
     $page = sanitize_key( $_GET['page'] );
-    $current = isset( $_GET['tab'] ) ? sanitize_key( $_GET['tab'] ) : $default_tab;
+    $current = isset( $_GET['tab'] ) ? sanitize_key( $_GET['tab'] ) : key( $tabs );
 
     // Tabs html
     $admin_tabs = '<div id="icon-edit-pages" class="icon32"><br /></div>';
     $admin_tabs .= '<h2 class="nav-tab-wrapper">';
         foreach( $tabs as $tab => $name ) {
             $class = ( $tab == $current ) ? ' nav-tab-active' : '';
-            $admin_tabs .= '<a href="?page='. $page .'&amp;tab='. $tab .'" class="nav-tab'. $class .'">'. $name .'</a>';
+            $admin_tabs .= '<a href="?page='. esc_attr( $page ) .'&amp;tab='. esc_attr( $tab ) .'" class="nav-tab'. esc_attr( $class ) .'">'. esc_attr__( $name ) .'</a>';
         }
     $admin_tabs .= '</h2><br />';
 
-    return $admin_tabs;
+    //echo $admin_tabs; /** use for do_action */
+    return $admin_tabs; /** use for echo function() */
 }
